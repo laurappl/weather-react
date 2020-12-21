c3a9f0c4bc8b65b4867a8cd766d6c3dbbd8e2935
laurappl <71591914+laurappl@users.noreply.github.com>
2020-12-21 11:35:13 +0100

    tested repository

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hello World blabla</h1>
+       <h1>Hello World </h1>
       </header>
     </div>
   );
